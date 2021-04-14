a387d8e05b020b757042b078c106f56aeb2298a4
Majorbig <caiquez.kirilo@hotmail.com>
2021-04-14 08:39:32 -0300

    teste aula

---
+++ Cursos/SENAI/API filmes/senai_filmes_webApp/senai_filmes_webApp/Domains/GeneroDomain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webApp.Domains
+{
+    /// <summary>
+    /// Classe que representa a entendidade (tabela) Generos
+    /// </summary>
+    public class GeneroDomain
+    {
+        public int idGenero { get; set; }
+        public string nome { get; set; }
+    }
+}
---
+++ Cursos/SENAI/API filmes/senai_filmes_webApp/senai_filmes_webApp/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,18 @@
+﻿using senai_filmes_webApp.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webApp.Interfaces
+{
+    /// <summary>
+    /// Interface responsável pelo repositório GeneroRepository
+    /// </summary>
+    public interface IGeneroRepository // se colocar apenas interface IGeneroRepository ele fica private como default
+    {
+         // TipoRetorno NomeMetodo(TipoParametro NomeParamentro); //pode não ter parametro
+
+        List<GeneroDomain>
+    }
+}
